adbb3006f80b527c25e4f72cb1f22c6a2de56015
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-07 11:24:57 -0500

    install firebase pod

--- FirebaseAuthDemo/FirebaseAuthDemo/AppDelegate.swift
+++ FirebaseAuthDemo/FirebaseAuthDemo/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  FirebaseAuthDemo
-//
-//  Created by Benjamin Stone on 11/7/19.
-//  Copyright © 2019 Benjamin Stone. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()        
         return true
     }
 
